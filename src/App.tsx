a5a7880c2e890e921f1b00c09ccc3b16ee883167
feliperdamaceno <feliperdamaceno@sent.com>
2024-10-13 00:42:45 +0100

    :zap: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components'
+
+export default function App() {
+  return (
+    <main>
+      <h1>Webpack React TypeScript</h1>
+      <Button>Click Me</Button>
+    </main>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const Webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts|jsx|js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new Webpack.ProvidePlugin({
+      React: 'react'
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    compress: true,
+    port: 3000
+  }
+}
